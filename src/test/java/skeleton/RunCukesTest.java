83a54565a7184f5cb853a0f3799e485ade3e672b
Biridepalli <harshitabiridepalli@gmail.com>
2019-08-23 15:24:31 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -6,11 +6,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Selenium_WS\\CucumberCs\\CS_Feature\\Feature13.feature", 
-glue="cucum_Pack1", tags= {"@Sanity"},
-plugin = {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+@CucumberOptions(tags= {"@Sanity"},plugin = {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
 
-public class RunnerCaseStudy {
+public class RunCukesTest {
 	
 
 }
